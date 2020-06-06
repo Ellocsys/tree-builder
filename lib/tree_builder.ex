b1b6c0585651fedc1e7f0eff4990aad7b4ff6840
Dmitriy Egorov <dmitriy.egorov@binomo.com>
2020-06-06 21:07:40 +0300

    add parser

--- lib/tree_builder.ex
+++ lib/tree_builder.ex
@@ -1,14 +1,56 @@
 defmodule TreeBuilder do
+  alias TreeBuilder.Node
+
   @moduledoc """
   Documentation for `TreeBuilder`.
   """
 
-  def tree(data) when is_bitstring(data) do
-    String.split(data, ",")
-    |> Enum.map(&String.split(&1, "/"))
+  def start(file) do
+    File.read!(file)
+    |> find_category()
   end
 
-  def parse_url(url) when is_list(url) do
-    nil
+  def find_category(data) when is_bitstring(data) do
+    data
+    |> String.split("\n")
+    |> Enum.map(&Regex.replace(~r/^http[[:alpha:]]?:\/\//, &1, ""))
+    |> Enum.map(&String.split(&1, "\/", parts: 2, trim: true))
+    |> List.delete_at(-1)
+    |> Enum.group_by(&List.first/1, &List.last/1)
+    |> Enum.map(&find_category/1)
   end
+
+
+  # def find_category({nil, [nil]}) do
+  #   %{}
+  # end
+
+  def find_category({domain, urls}) do
+    category = urls
+    |> Enum.map(&String.split(&1, "\/", trim: true))
+    |> Enum.map(&List.delete_at(&1, -1))
+    |> Enum.map(&Enum.join(&1, "/"))
+    |> Enum.filter(fn url -> String.length(url) > 0 end)
+    |> Enum.dedup()
+    Map.put(%{}, domain, category)
+    |> IO.inspect()
+  end
+  # def parse_url(url, acc) do
+  #   [domain | data ] = url
+  #   |> String.split("\/")
+
+  #   data
+  #   |> Enum.reduce(%Node{name: domain}, &build_node/2)
+  # end
+
+  # def build_node([node_name, data], %Node{children: children}) do
+  #   children
+  #   |> Enum.find(%Node{name: node_name}, fn node -> Map.get(node, :name) == node_name end)
+  #   |> Map.update(:children, [], &build_node())
+  # end
+
+  # def build_node({start_id, nodes} = acc, node_name, parent) do
+  #   id = start_id + 1
+  #   {id, [%Node{id: id, name: node_name}]}
+  # end
 end
--- test/tree_builder_test.exs
+++ test/tree_builder_test.exs
@@ -1,14 +1,55 @@
 defmodule TreeBuilderTest do
   use ExUnit.Case
+
   # doctest TreeBuilder
 
+  alias TreeBuilder.Node
+
+  # test "build tree" do
+  #   tree = "vk.com/users/friends/kryglokot,vk.com/users/friends/radzivonchick"
+
+  #   assert TreeBuilder.tree(tree) == [
+  #            %Node{
+  #              name: "vk.com",
+  #              children: [
+  #                %Node{
+  #                  name: "users",
+  #                  children: [
+  #                    %Node{
+  #                      name: "friends",
+  #                      children: [
+  #                        %Node{name: "kryglokot", children: []},
+  #                        %Node{name: "radzivonchick", children: []}
+  #                      ]
+  #                    }
+  #                  ]
+  #                }
+  #              ]
+  #            }
+  #          ]
+  # end
+
   test "build tree" do
-    tree = "vk.com/users/friends/kryglokot,vk.com/users/friends/radzivonchick"
-    assert TreeBuilder.tree(tree) == [{1, "vk.com", nil}, {2, "users", 1}, {3, "friends", 2}, {4, "kryglokot", 3}, {5, "radzivonchick", 3}]
+    tree = "vk.com/users/friends/kryglokot,vk.com/users/friends/radzivonchick,vk.com/users/friends,vk.com/users,vk.com/albums,vk.com/albums/private"
+
+    # %{"vk.com" => [["users", "friends", "kryglokot"], ["users", "friends", "radzivonchick"], ["users"]]}
+    assert TreeBuilder.find_category(tree) == [%{"vk.com" => ["users/friends", "users", "albums"]}]
   end
 
-  test "parse url" do
-    url_list = ["vk.com"]
-    assert TreeBuilder.parse_url(url_list) == [{1, "vk.com", nil}]
+  test "build tree" do
+    tree = "vk.com/users/friends/kryglokot,vk.com/users/friends/radzivonchick,vk.com/users/friends,vk.com/users,vk.com/albums,vk.com/albums/private"
+
+    # %{"vk.com" => [["users", "friends", "kryglokot"], ["users", "friends", "radzivonchick"], ["users"]]}
+    assert TreeBuilder.find_category(tree) == [%{"vk.com" => ["users/friends", "users", "albums"]}]
   end
+
+  # test "parse url" do
+  #   url_list = ["vk.com"]
+  #   assert TreeBuilder.parse_url(url_list) == [{1, "vk.com", nil}]
+  # end
+
+  # test "build node" do
+  #   ["vk.com", "users", "friends", "kruglokot"]
+  #   assert false
+  # end
 end
